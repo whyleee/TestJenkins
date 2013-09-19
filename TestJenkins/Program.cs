3c0903b8e38045fff2d0807bbc48121155faaae9
Pavel Nezhencev <whyleee@gmail.com>
2013-09-20 00:49:53 +0300

    Yup

--- TestJenkins/Program.cs
+++ TestJenkins/Program.cs
@@ -11,7 +11,7 @@ namespace TestJenkins
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Jenkins!");
-            // new line // another change // and another one
+            // new line // another change // and another one // yup
         }
     }
 }
